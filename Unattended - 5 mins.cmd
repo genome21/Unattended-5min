9764ba94b31cfa414cba38c4902ecf2da7441eee
Robert Wilkins III <thecainvestors@gmail.com>
2017-12-07 14:03:27 -0600

    Add files via upload
    Added the Unattanded (5 mins) script

---
+++ Unattended - 5 mins.cmd
@@ -0,0 +1,21 @@
+@echo off
+rundll32 user32.dll SetCursorPos 0 0
+TIMEOUT /T 30 /nobreak
+rundll32 user32.dll SetCursorPos 0 100
+TIMEOUT /T 30 /nobreak
+rundll32 user32.dll SetCursorPos 0 0
+TIMEOUT /T 30 /nobreak
+rundll32 user32.dll SetCursorPos 0 100
+TIMEOUT /T 30 /nobreak
+rundll32 user32.dll SetCursorPos 0 0
+TIMEOUT /T 30 /nobreak
+rundll32 user32.dll SetCursorPos 0 100
+TIMEOUT /T 30 /nobreak
+rundll32 user32.dll SetCursorPos 0 0
+TIMEOUT /T 30 /nobreak
+rundll32 user32.dll SetCursorPos 0 100
+TIMEOUT /T 30 /nobreak
+rundll32 user32.dll SetCursorPos 0 0
+TIMEOUT /T 30 /nobreak
+rundll32 user32.dll SetCursorPos 0 100
+EXIT
